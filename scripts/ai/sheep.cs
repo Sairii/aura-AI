65643b16759568d9de0ae9ec4090d158dc312d3c
AinnaYui <ainna@hotmail.de>
2015-11-17 23:02:31 +0100

    baaaah you formatting

--- scripts/ai/sheep.cs
+++ scripts/ai/sheep.cs
@@ -7,36 +7,36 @@
 [AiScript("sheep")]
 public class SheepAi : AiScript
 {
-	public SheepAi()
-	{
-		// visual range 400 - 90°, audio range 300
-		SetAggroRadius(400);
-		
-		Hates("/wolf/");
-	}
-	
-	protected override IEnumerable Idle()
-	{
-		Do(Wander(100, 400));
-		Do(Wait(10000, 14000));
-	}
-	
-	protected override IEnumerable Alert()
-	{
-		var rndAlert = Random();
+    public SheepAi()
+    {
+        // visual range 400 - 90°, audio range 300
+        SetAggroRadius(400);
+
+        Hates("/wolf/");
+    }
+
+    protected override IEnumerable Idle()
+    {
+        Do(Wander(100, 400));
+        Do(Wait(10000, 14000));
+    }
+
+    protected override IEnumerable Alert()
+    {
+        var rndAlert = Random();
         if (rndAlert < 25)
         {
             Do(PrepareSkill(SkillId.Defense));
         }
-		Do(Wait(2000, 4000));
-		Do(CancelSkill());
-		Do(KeepDistance(800, false));
-		
-	}
-	
-	protected override IEnumerable Aggro()
-	{
-		Do(Attack(3));
-		Do(Wait(3000, 8000));
-	}
+        Do(Wait(2000, 4000));
+        Do(CancelSkill());
+        Do(KeepDistance(800, false));
+
+    }
+
+    protected override IEnumerable Aggro()
+    {
+        Do(Attack(3));
+        Do(Wait(3000, 8000));
+    }
 }
